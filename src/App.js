9b0893487bae1b57a053d4830c3b3e7abe9a42cd
Meet Solanki <meet@ayelite.com>
2024-10-25 11:29:47 +0530

    Added bg changer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import ChildA from "./components/ChildA";
+import './App.css'
+// step1 : Create context
+// const UserContext = createContext()
+// step2: wrap in provider 
+// step3: pass value in provide
+	
+const ThemeContext = createContext()
 
 function App() {
+
+  const [theme, setTheme] = useState('light')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<ThemeContext.Provider value={{theme,setTheme}}>
+			<div className="container" style={{backgroundColor:theme==='light'?'white':'black'}}>
+				<ChildA/>
+			</div>
+		</ThemeContext.Provider>
+    </>
   );
 }
 
 export default App;
+export {ThemeContext}
